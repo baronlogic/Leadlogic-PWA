28da8509c8209ab559157390d51b3f952edd87dd
Rafael Baron <baronrafael@hotmail.com>
2019-09-25 16:48:28 -0400

    feat: configurating service

--- src/app/core/services/device-scans.service.ts
+++ src/app/core/services/device-scans.service.ts
@@ -8,4 +8,12 @@ const ENDPOINT_NAME = 'DeviceScans';
 })
 export class DeviceScansService extends ApiService {
 
+  saveScanRecord(clientId: string, projectId: string, scanRecord: any){
+    return this.http.post(this.API_URL+clientId+'-'+projectId+'-2-0/'+ENDPOINT_NAME, scanRecord, this.httpOptions);
+  }
+
+  getAllScans(clientId: string, projectId: string, personId: string){
+    return this.http.get(this.API_URL+clientId+'-'+projectId+'-2-0/'+ENDPOINT_NAME+'/'+personId, this.httpOptions);
+  }
+
 }

9ef109e5dd427f10702b823f08177b0b89ee0a3d
Rafael Baron <baronrafael@hotmail.com>
2019-09-25 20:30:39 -0400

    feat: creating new table

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,96 +7,7 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { PersonsService } from '../../core/services/persons.service';
 import { DeviceScansService } from '../../core/services/device-scans.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
-  }, {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li',
-    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
-  }, {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`
-  }, {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`
-  }, {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`
-  }, {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
-  }, {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`
-  }, {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`
-  }, {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`
-  },
-];
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -120,8 +31,8 @@ export class DashboardComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   dataSource: any;
-  columnsToDisplay = ['Family Name', 'First Name', 'Company'];
-  expandedElement: PeriodicElement | null;
+  columnsToDisplay = ['Family_Name', 'First_Name', 'Company'];
+  expandedElement: any | null;
 
   constructor(
     private router: Router,
@@ -135,7 +46,7 @@ export class DashboardComponent implements OnInit {
       this.goToLogin();
       return;
     }
-    this.user = JSON.parse(localStorage.getItem('userLogged'));
+    this.user = JSON.parse(localStorage.getItem('leadLogged'));
     this.getUserData(this.user.clientId, this.user.projectId, this.user.personId);
     this.getTheContacts(this.user.clientId, this.user.projectId, this.user.personId);
   }
@@ -166,14 +77,21 @@ export class DashboardComponent implements OnInit {
   }
 
   getTheContacts(clientId: string, projectId: string, personId: string){
-    this.deviceScansService.getAllScans(clientId, projectId, personId)
+    this.deviceScansService.getAllScans(clientId, projectId, personId).pipe(
+      map(
+        (resp: any) => { 
+          return resp.map(({Family_Name, First_Name, Company, Job_Title, Email, Mobile}) =>
+            ({Family_Name, First_Name, Company, Job_Title, Email, Mobile}));
+        }
+      )
+    )
     .subscribe(
       res => {
         this.contactsData = res;
         this.dataSource = new MatTableDataSource(this.contactsData);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-        //console.log(this.dataSource.filteredData);
+        console.log(this.dataSource.filteredData);
         //console.log(this.contactsData);
       },
       err => {

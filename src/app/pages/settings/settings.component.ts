d11e5d7602defc03e1ace7ff8842a5af71356bf0
Rafael Baron <baronrafael@hotmail.com>
2019-11-12 12:10:27 -0400

    feat: add notes to leads spreadsheet

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ExcelService } from 'src/app/core/services/excel.service';
 import { DeviceScansService } from 'src/app/core/services/device-scans.service';
 import { map } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
@@ -58,31 +59,61 @@ export class SettingsComponent implements OnInit {
 
   exportAsXLSX(data):void {
     this.excelService.exportAsExcelFile(data, this.user.clientId);
- }
+  }
 
- getLeads(){
-  this.bLeads = true;
-  this.deviceScansService.getAllScans(this.user.clientId, this.user.projectId, this.user.personId).pipe(
-    map(
-      (resp: any) => { 
-        return resp.map(({Last_Scanned, Prefix_Title, First_Name, Family_Name, Job_Title, Company, Address_1, Zip_Code, City, Country_Code, Email, Mobile}) =>
-          ({Last_Scanned, Prefix_Title, First_Name, Family_Name, Job_Title, Company, Address_1, Zip_Code, City, Country_Code, Email, Mobile}));
-      }
+  getLeads(){
+    this.bLeads = true;
+    this.deviceScansService.getAllScans(this.user.clientId, this.user.projectId, this.user.personId).pipe(
+      map(
+        (resp: any) => { 
+          return resp.map(({Person_Id, Last_Scanned, Prefix_Title, First_Name, Family_Name, Job_Title, Company, Address_1, Zip_Code, City, Country_Code, Email, Mobile}) =>
+          ({Person_Id, Last_Scanned, Prefix_Title, First_Name, Family_Name, Job_Title, Company, Address_1, Zip_Code, City, Country_Code, Email, Mobile}));
+        }
+      )
     )
-  )
-  .subscribe(
-    res => {
-       //console.log(res);
-       this.leads = res;
-       this.exportAsXLSX(this.leads);
-       this.bLeads = false;
-    },
-    err => {
-      console.log(err);
-      this.bLeads = false;
-      //this.openSnackBar(err.message);
+    .subscribe(
+      res => {
+        //console.log(res);
+        this.leads = res;
+        this.getLeadsNotes();
+      },
+      err => {
+        console.log(err);
+        //this.openSnackBar(err.message);
+      }
+    );
+  }
+
+  getNotes(element){
+    return this.deviceScansService.getNotesForAPerson(this.user.clientId, this.user.projectId, element.Person_Id, this.user.personId)
+  }
+
+  getLeadsNotes(){
+    let aux = [];
+    for(let i = 0; i < this.leads.length; i++){
+      aux.push(this.getNotes(this.leads[i]));
     }
-  );
-}
+    forkJoin(aux).subscribe(
+      res => {
+        let auxNotes: any = res;
+        for(let i = 0; i < this.leads.length; i++){
+          if(auxNotes[i] != null){
+            this.leads[i].Notes = auxNotes[i].Notes;
+          }
+          else{
+            this.leads[i].Notes = '';
+          }
+        }
+        //console.log(this.leads);
+        this.exportAsXLSX(this.leads);
+        this.bLeads = false;
+      },
+      err => {
+        console.log(err);
+        this.bLeads = false;
+        //this.openSnackBar(err.message);
+      }
+    );
+  }
 
 }

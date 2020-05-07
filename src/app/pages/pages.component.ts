1ac3a570a235aed42a3c2a4bd10efca3386af60d
Rafael Baron <baronrafael@hotmail.com>
2020-05-07 06:22:06 -0400

    feat: bottom nav for mobile devices

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FocusMonitor } from '@angular/cdk/a11y';
 import { Router } from '@angular/router';
+import {BottomNavItem} from 'ngx-bottom-nav';
 
 @Component({
   selector: 'app-pages',
@@ -8,6 +9,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent implements OnInit {
+
+  items: BottomNavItem[] = [
+    {icon: 'home', label: 'Home', routerLink: '/pages/home'},
+    {icon: 'center_focus_weak', label: 'Scan', routerLink: '/pages/scan'},
+    {icon: 'settings', label: 'Settings', routerLink: '/pages/settings'},
+  ];
 
   constructor(
     private router: Router,
